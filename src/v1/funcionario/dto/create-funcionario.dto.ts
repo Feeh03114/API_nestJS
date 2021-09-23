bad6b072e530b830831e1e9922b05dd1da62d241
felipe ales <felipe03114@gmail.com>
2021-09-23 20:28:55 -0300

    Updated

--- src/v1/funcionario/dto/create-funcionario.dto.ts
+++ src/v1/funcionario/dto/create-funcionario.dto.ts
@@ -1,5 +1,6 @@
 import { Endereco } from 'src/v1/endereco/entities/endereco.entity';
 import { Telefone } from 'src/v1/telefone/entities/telefone.entity';
+import { User } from 'src/v1/users/entities/user.entity';
 
 export class CreateFuncionarioDto {
   Name: string;
@@ -7,4 +8,5 @@ export class CreateFuncionarioDto {
   endereco_id: string;
   endereco: Endereco;
   telefones: Telefone[];
+  Usuario?: User;
 }
--- src/v1/users/users.service.ts
+++ src/v1/users/users.service.ts
@@ -7,6 +7,7 @@ import { User } from './entities/user.entity';
 import { Funcionario } from '../funcionario/entities/funcionario.entity';
 import { Endereco } from '../endereco/entities/endereco.entity';
 import { Telefone } from '../telefone/entities/telefone.entity';
+import { Cargo } from '../cargo/entities/cargo.entity';
 
 @Injectable()
 export class UsersService {
@@ -43,13 +44,10 @@ export class UsersService {
     const { id: id_funcionario } = funcionarioSaved;
 
     // add telefone ao funcionario
-
     const telefoneRepository = getRepository(Telefone);
     telefones.forEach(async (telefone_item) => {
       const telephone = { ...telefone_item, funcionario_id: id_funcionario };
 
-      /* telefone_item
-      funcionario_id: id_funcionario,  */
       const telefoneCreated = telefoneRepository.create(telephone);
 
       await telefoneRepository.save(telefoneCreated);
@@ -75,17 +73,51 @@ export class UsersService {
     return this.usersRepository.find();
   }
 
-  findOne(id: string) {
-    return this.usersRepository.findOne(id);
+  async findOne(id: string) {
+    const funcionarioRepository = getRepository(Funcionario);
+    const enderecoRepository = getRepository(Endereco);
+    const cargoRepository = getRepository(Cargo);
+    const telefoneRepository = getRepository(Telefone);
+
+    const get_user = await this.usersRepository.findOne(id);
+    const get_funcionario = await funcionarioRepository.findOne(
+      get_user.funcionario_id,
+    );
+    const endereco = await enderecoRepository.findOne(
+      get_funcionario.endereco_id,
+    );
+    const cargo = await cargoRepository.findOne(get_funcionario.cargo_id);
+    const telefones = await telefoneRepository.find({
+      select: ['id', 'Telefone'],
+      where: { funcionario_id: get_funcionario.id },
+    });
+    const funcionario = {
+      ...get_funcionario,
+      endereco: endereco,
+      Cargo: cargo,
+      telefones: telefones,
+    };
+    delete funcionario.cargo_id;
+    delete funcionario.endereco_id;
+
+    const user = {
+      ...get_user,
+      funcionario: funcionario,
+    };
+
+    delete user.funcionario_id;
+    return user;
   }
 
-  update(id: string, updateUserDto: UpdateUserDto) {
-    this.usersRepository.update(id, updateUserDto);
-    return updateUserDto;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    await this.usersRepository.update(id, updateUserDto);
+    const userUpdate = await this.usersRepository.findOne(id);
+    delete userUpdate.Password;
+    return userUpdate;
   }
 
   async remove(id: string) {
     await this.usersRepository.delete(id);
-    return 'Usuário removido';
+    return { Status: 'Usuário removido' };
   }
 }
